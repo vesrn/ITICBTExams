11efbcf29c443c305fe2d1ca96dd9ee5b73b298f
vesrn <64141054+vesrn@users.noreply.github.com>
2024-06-14 12:23:33 +0530

    questions.js

--- js/questions.js
+++ js/questions.js
@@ -91,7 +91,238 @@ let questions = [
       "Prohibition sign"
     ]
   },
-
+      {
+    numb: 9,
+    question: "What is the back ground colour of warning signs in the basic category? ",
+    
+    answer: "Yellow",
+    options: [
+      "Blue",
+      "White",
+      "Yellow",
+      "Green"
+    ]
+  },
+    {
+    numb: 10,
+    question: " Which type of fire extinguisher is used for fire on electrical equipment?",
+    
+    answer: "Halon type",
+    options: [
+      "Halon type",
+      "Foam type ",
+      "Gas cartridge type",
+      "Stored pressure type"
+    ]
+  },
+     {
+    numb: 11,
+    question: " Which is the waste disposal method that produces heat?",
+    
+    answer: "Incineration",
+    options: [
+      "Recycling",
+      "Composting",
+      "Incineration",
+      "Waste compaction "
+    ]
+  },
+     {
+    numb: 12,
+    question: " Which Personal Protective Equipment PPE is used for the protection from fumes?",
+    
+    answer: "Nose mask",
+    options: [
+      "Apron",
+      "Goggles",
+      "Ear mask ",
+      "Nose mask "
+    ]
+  },
+ {
+    numb: 13,
+    question: " What is the full form of BIS?",
+    
+    answer: "Bureau of Indian Standard",
+    options: [
+      "Board of Indian Standard",
+      "Bureau of Indian Standard",
+      "Board of International Standard",
+      "Bureau of International Standard"
+    ]
+  },
+{
+    numb: 14,
+    question: "Which method is used to lift and move heavy loads?",
+    
+    answer: "Crane and slings",
+    options: [
+      "Winches",
+      "Crane and slings",
+      "Layers and Rollers",
+      "Machine moving platforms"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is starving in extinguishing of fire?",
+    
+    answer: "Removing fuel element from the fire",
+    options: [
+      "Adding fuel to the fire",
+      "Using water to cool the fire",
+      "Removing fuel element from the fire",
+     "Preventing oxygen supply to the fire"
+    ]
+  },
+    {
+    numb: 16,
+    question: "What is the name of PPE? ",
+     immg:"img16",
+    answer: "Hand screen",
+    options: [
+      "Nose mask",
+      "Head shield",
+      "Face shield",
+      "Hand screen"
+    ]
+  },
+    {
+    numb: 17,
+    question: " Which disposal method of waste save lot of energy?",
+    
+    answer: "Recycling ",
+    options: [
+      "Land fill",
+      "Recycling ",
+      "Incineration",
+      "Composting"
+    ]
+  },
+     {
+    numb: 18,
+    question: " Which artificial respiration method is to be performed to the victim with injuries on the 
+chest and belly? ",
+    
+    answer:"Nelson’s arm-lift back pressure method",
+    options: [
+      "Schafer’s method",
+      "Mouth to mouth method ",
+      "Mouth to nose method",
+      "Nelson’s arm-lift back pressure method"
+    ]
+  },
+       {
+    numb: 19,
+    question: "Which type of occupational health hazard is 
+cause for infection? ",
+    
+    answer:"Biological hazard",
+    options: [
+      "Electrical hazard",
+      "Biological hazard",
+      "Physiological hazard",
+      "Psychological hazard"
+    ]
+  },
+        {
+    numb: 20,
+    question: " What is the use of this tool? ",
+    immg:"img20",
+    answer:"Extracting nails from the wood",
+    options: [
+      "Holding the hot substances",
+      "Cutting and twisting the wires",
+      "Extracting nails from the wood",
+      "Loosening and tightening the bolts and nuts"
+    ]
+  },  
+             {
+    numb: 21,
+    question: " How will you diagnose the victim is suffering under cardiac arrest?",
+    
+    answer:"Appears blue colour around his lips",
+    options: [
+      "Gets pain in spinal guard",
+      "Mouth will be closed tightly ",
+      "Heavy swelling on his stomach",
+      "Appears blue colour around his lips"
+    ]
+  },
+                 
+        {
+    numb: 22,
+    question: " What is the name of the tool? ",
+    immg:"img22",
+    answer:"Diagonal cutting pliers",
+    options: [
+      "Wire stripper",
+      "Crimping tool",
+      "Combination pliers",
+      "Diagonal cutting pliers"
+    ]
+  },  
+     {
+    numb: 23,
+    question: "What is the name of the safety sign?  ",
+    immg:"img23",
+    answer:"Information sign",
+    options: [
+      "Warning sign",
+      "Mandatory sign",
+      "Prohibition sign",
+      "Information sign"
+    ]
+  }, 
+     {
+    numb: 24,
+    question: "What will be first-aid to be given to stop the bleeding of the victim?",
+    
+    answer: "Applying pressure over the injured portion",
+    options: [
+      "Applying ointment",
+      "Keep the injured portion upward",
+      "Covering the wound portion by dressing",
+     "Applying pressure over the injured portion"
+    ]
+  },
+         
+               {
+    numb: 26,
+    question: "What is the name of the cautionary sign?  ",
+    immg:"img26",
+    answer:"Pedestrian crossing",
+    options: [
+      "School",
+      "Guarded",
+      "Unguarded ",
+      "Pedestrian crossing"
+    ]
+  }, 
+     {
+    numb: 27,
+    question: " Which is the golden hour for the victim injured on head with risk of dying?",
+    
+    answer: "First 30 minutes ",
+    options: [
+      "First 15 minutes",
+      "First 30 minutes",
+      "First 45 minutes",
+     "First 60 minutes "
+    ]
+  },
+          {
+    numb: 28,
+    question: "Which condition of the victim is referred as COMA stage? ",
+    
+    answer: "Lie totally senseless and do not respond to calls ",
+    options: [
+      "Unconscious but can respond to calls",
+      "Conscious but cannot respond to calls ",
+      "Breathing but cannot respond to calls",
+     "Lie totally senseless and do not respond to calls"
+    ]
+  }
     
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
