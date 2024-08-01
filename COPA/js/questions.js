ebcbecb7594dd96582c3c6ea84650f6684c338c1
vesrn <64141054+vesrn@users.noreply.github.com>
2024-08-01 22:12:37 +0530

    Add files via upload

---
+++ COPA/js/questions.js
@@ -0,0 +1,110 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of the safety sign?",
+    immg:"img1",
+    answer: "Information Sign",
+    options: [
+      "Warn Sign",
+      "Mandatory Sign",
+      "Prohibition Sign",
+      "Mandatory Sign"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is smothering in extinguishing of fire?",
+    answer: "Isolating the fire from the supply of oxygen ",
+    options: [
+      "Adding the fuel element to the fire",
+      "Removing the fuel element from the fire",
+      "Using of water to lower the temperature", 
+      "Isolating the fire from the supply of oxygen"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the physical hazard?",
+    
+    answer: "Vibration",
+    options: [
+      "Smoking",
+      "Vibration",
+      "Corrosive",
+      "Radio Active"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the correct sequence of operation to be performed when using the fire extinguisher?",
+    answer: "Pull, Aim, Squeeze,Sweep",
+    options: [
+      "Pull, Aim, Squeeze,Sweep",
+      "Pull, Aim, Sweep,Squeeze",
+      "Push, Arrange,Squeeze, Sweep ",
+      "Push, Arrange,Sweep, Sequence"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which step of the 5s-concept refers 'Standardization' ?",
+    answer: "Step-4",
+    options: [
+      "Step-1",
+      "Step-2",
+      "Step-3",
+      "Step-4"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which plier is used for making wire hooks and loops?",
+    answer: "Round nose plier",
+    options: [
+      "Flat nose plier",
+      "Long nose plier",
+      "Round nose plier",
+      "Diagonal cutting plier"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the use of pincer? ",
+    answer: "Extracting the pin nails from the wood",
+    options: [
+      "Twisting the flexible wires ",
+      "Cutting small diameter of wires",
+      "Extracting the pin nails from the wood",
+      "Holding small objects, where fing"
+    ]
+  },
+  {
+    numb: 8,
+    question: "  What is the name of road safety sign? ",
+    immg:"img8",
+    answer: "Mandatory sign ",
+    options: [
+      "Mandatory sign",
+      "Cautionary sign",
+      "Informatory sign ",
+      "Prohibition sign"
+    ]
+  },
+  
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
